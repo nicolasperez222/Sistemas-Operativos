f089200c6b567064564350ffd570cc8b93dbcee3
Nicolás Sebastián Pérez Trujillo <116771323+nicolasperez222@users.noreply.github.com>
2024-08-15 00:51:50 -0500

    Add files via upload
    Código Primera vez usando CoCalc

---
+++ Cap01/2024-08-01-file-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* 
+    Pontificia Universidad Javeriana
+    Nicolas Sebastian Perez Trujillo
+    Sistemas Operativos
+    Codigo 01
+    Fecha: 01/08/2024
+*/
+
+int main() {
+    printf("Tarjeta de Identificación\n");
+    printf("------------------------\n");
+    printf("Nombre: Nicolás Sebastián Pérez Trujillo\n");
+    printf("Semestre: 4\n");
+    printf("Edad: 20\n");
+    printf("Materias:\n");
+    printf("  Estructura de Datos\n");
+    printf("  Finanzas Básicas\n");
+    printf("  Fundamentos de Ing. de SW\n");
+    printf("  Sistemas Operativos\n");
+    printf("  Gestión de Innovación en TI\n");
+
+    return 0;
+}
