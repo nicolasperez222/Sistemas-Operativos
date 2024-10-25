61574eeacd4b90abfa9c46970d86988bc3167c74
Nicolás Sebastián Pérez Trujillo <116771323+nicolasperez222@users.noreply.github.com>
2024-10-24 22:21:02 -0500

    Update prod_cons_posix.c

--- Posix/prod_cons_posix.c
+++ Posix/prod_cons_posix.c
@@ -1,3 +1,13 @@
+/**************************************
+
+  Pontificia Universidad Javeriana 
+  Tema: Máquina Virtual
+  Tópico: Posix
+  Autor: Nicolás S. Pérez
+  Materia: Sistemas Operativos
+  
+**************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
